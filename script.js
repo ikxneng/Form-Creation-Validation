b36376f52c20ee609e2f6d4f47caa41d46f45f9b
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-07-17 14:22:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+
+    let isValid = true;
+    
+    let messages = [];
+
+    if (username.length < 3){
+        isValid = false;
+        messages.push("Your username should be at least 3 characters.");
+    }
+
+    if (!email.includes('@') || !email.includes('.')){
+        isValid = false;
+        messages.push("Your email address is invalid. Please make sure it includes '@' and '.'.");
+    }
+
+    if (password.length < 8){
+        isValid = false;
+        messages.push("Please make sure your password has at least 8 characters. ");
+    };
+
+    feedbackDiv.style.display ='block';
+
+    if (isValid){
+        feedbackDiv.textContent = "Registration succesful!";
+        feedbackDiv.style.color = "#28a745";
+    } else {
+        feedbackDiv.innerHTML = messages.join('<br>');
+        feedbackDiv.style.color = "#dc3545";
+    }
+    });
+});
